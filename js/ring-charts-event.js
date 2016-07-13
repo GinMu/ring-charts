e7ef159b20d4de34828a09d044d0f40b52eff481
GinMu <smu@anfengde.com>
2016-07-13 13:57:08 +0800

    SPA页面

--- js/ring-charts-event.js
+++ js/ring-charts-event.js
@@ -1,5 +1,6 @@
 (function($, undefined) {
-    $('.nav li').off('click').on('click', function() {
+    $('#nav-data li').off('click').on('click', function() {
+        $('#ring-charts').removeClass('hidden');
         var self = this;
         var type = self.getAttribute('type');
         $('li.active').removeClass('active');
@@ -10,6 +11,42 @@
         var title = dropdownMenu.text();
         var filename = dropdownMenu.attr("filename");
         $.logCharts(type + '-title', title, filename);
+    });
+    $('#nav-region li').off('click').on('click',function(){
+      $('#ring-charts').removeClass('hidden');
+      var self = this;
+      var type = self.getAttribute('type');
+      $('li.active').removeClass('active');
+      $(self).addClass('active');
+      $('.shown').removeClass('shown').addClass('hidden');
+      $('#' + type).removeClass('hidden').addClass('shown');
+      var currentDate = new Date(+new Date() - 16 * 3600 * 1000).toISOString().substring(0,10);
+			$('#dateTime').val(currentDate);
+      $.logsMaps(currentDate);
+    });
+    $('#nav-uuid li').off('click').on('click',function(){
+      $('#ring-charts').addClass('hidden');
+      $('#ring-uuid').removeClass('hidden');
+      var self = this;
+      var type = self.getAttribute('type');
+      $('li.active').removeClass('active');
+      $(self).addClass('active');
+      $('.shown').removeClass('shown').addClass('hidden');
+      $('#' + type).removeClass('hidden').addClass('shown');
+      var currentDate = new Date(+new Date() - 16 * 3600 * 1000).toISOString().substring(0,10);
+      $('#uuid-dateTime').val(currentDate);
+    });
+    $('#uuid-query').off('click').on('click',function(){
+      var time = $('#uuid-dateTime').val();
+      var uuid = $('#uuid-text').val();
+      if (!time || !uuid) {
+        alert('请输入时间或UUID');
+        return;
+      }
+      if ($('#ol_list li').length > 0) {
+        $('#ol_list').empty();
+      }
+      $.searchUUID(time,uuid);
     });
     $("ul.dropdown-menu li").off("click").on("click", function() {
         var self = this;
@@ -39,4 +76,8 @@
         var type = this.value;
         $.updateType(type);
     });
+    $('#query').off('click').on('click',function(){
+			var date = $('#dateTime').val();
+			$.logsMaps(date);
+		});
 }(jQuery, window));
